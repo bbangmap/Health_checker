3cde4aa55cf0f7c8127cc2edd4d0255074aaab5c
JunhyeonOh <Oh.junhyeon95@gmail.com>
2023-09-08 17:48:28 +0000

    mau

---
+++ Sources/amplitude_mau.py
@@ -0,0 +1,34 @@
+from amplitude_event_api import fetch_event_segmentation
+from tracking_slack import send_to_slack
+from datetime import datetime, timedelta
+
+# 현재 날짜 가져오기
+today = datetime.now()
+# 1일 전 날짜를 계산하기
+yesterday = today - timedelta(days=30)
+
+# 날짜를 YYYYMMDD 형식으로 변환
+start_date = yesterday.strftime('%Y%m%d')
+end_date = today.strftime('%Y%m%d')
+
+all_user_result = fetch_event_segmentation(
+    event_type="_all",
+    start_date=start_date,
+    end_date=end_date,
+    i=30
+)
+
+new_user_result = fetch_event_segmentation(
+    event_type="_new",
+    start_date=start_date,
+    end_date=end_date,
+    i=30
+)
+
+try:
+    new_value = new_user_result['data']['series'][0][0]
+    all_value = all_user_result['data']['series'][0][0]
+except ValueError:
+    print(f"No data available for {start_date}.")
+
+send_to_slack(f"{start_date}\n\nMAU : {all_value}\nNEW_USER : {new_value}")
